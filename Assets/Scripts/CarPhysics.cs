9f208124eff9166cb4b64ab23f6298096dda5f60
DESKTOP-1D6HVLS\krasi <krasi2405@gmail.com>
2017-04-15 02:20:35 +0300

    Refactoring and (hopefully) removed bug with car spiraling out of control whenever i brake at high speeds.

--- Assets/Scripts/CarPhysics.cs
+++ Assets/Scripts/CarPhysics.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarPhysics : MonoBehaviour {
+public class CarPhysics : MonoBehaviour
+{
 
     public float acceleration = 15f;
     public float angularDrag = 3f;
@@ -11,9 +12,7 @@ public class CarPhysics : MonoBehaviour {
     public float brake_force = 2f;
     public float hit_points = 100;
     public float max_speed = 100f;
-    public float brake_difference = 2f;
     public float torque_coefficient = 1f;
-    public float brake_turbulence_coefficient = 1f;
     public bool controlled_by_player = false;
 
     public KeyCode forward;
@@ -21,24 +20,24 @@ public class CarPhysics : MonoBehaviour {
     public KeyCode left;
     public KeyCode right;
 
-    public float curr_speed { get; private set;}
+    public float curr_speed { get; private set; }
 
     private Rigidbody2D rb;
     private bool gas;
-    private bool brake_left = true;
-    private float last_brake_tick;
     private ParticleSystem smoke;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         rb.mass = weight;
         rb.angularDrag = angularDrag;
         rb.drag = linearDrag;
+        rb.gravityScale = 0f;
         smoke = GetComponentInChildren<ParticleSystem>();
         smoke.Stop();
-	}
+    }
 
     void FixedUpdate()
     {
@@ -51,38 +50,20 @@ public class CarPhysics : MonoBehaviour {
         GameObject collidedWith = collision.gameObject;
         float force = 1f;
         float angle = Vector3.Angle(gameObject.transform.position - collidedWith.transform.position, gameObject.transform.up);
-        float hit_coefficient = 1f;
+        float hit_coefficient = GetHitCoefficient(angle);
 
         print("Angle between " + gameObject.name + " and " + collidedWith.name + " is : " + angle);
 
-        for (int i = 0; i < angle; i++)
-        {
-            if(i <= 30 || i >= 150)
-            {
-                hit_coefficient += 0.05f;
-            }
-            else if(i >= 60 && i <= 90)
-            {
-                hit_coefficient += 0.08f;
-            }
-            else if(i >= 90 && i <= 120)
-            {
-                hit_coefficient -= 0.08f;
-            }
-            else
-            {
-                hit_coefficient -= 0.05f;
-            }
-        }
+
         print("Hit coefficient is: " + hit_coefficient);
-        
+
         try
         {
             force =
                (collidedWith.GetComponent<CarPhysics>().curr_speed / 8 + curr_speed / 4) *
                 hit_coefficient;
         }
-        catch(MissingComponentException)
+        catch (MissingComponentException)
         {
             force = Mathf.Clamp(1 / rb.mass, 0.5f, 1.5f) * curr_speed / 4;
         }
@@ -96,25 +77,24 @@ public class CarPhysics : MonoBehaviour {
         print(gameObject.name + " Collided for " + force + " damage ");
 
 
-        if(hit_points <= 40) {
+        if (hit_points <= 40)
+        {
             smoke.Play();
         }
     }
 
     void BasicMovementControls()
     {
-
-        last_brake_tick += Time.deltaTime;
         float curr_velocity = rb.velocity.sqrMagnitude;
         if (controlled_by_player)
         {
             if (Input.GetKey(left))
             {
-                rb.AddTorque(GetTorqueDependingOnSpeed());
+                rb.AddTorque(GetTorque());
             }
             if (Input.GetKey(right))
             {
-                rb.AddTorque(-GetTorqueDependingOnSpeed());
+                rb.AddTorque(-GetTorque());
             }
             if (Input.GetKey(forward))
             {
@@ -136,55 +116,67 @@ public class CarPhysics : MonoBehaviour {
             {
                 if (curr_velocity > max_speed / 3)
                 {
-                    Vector3 brake_rotate = Vector3.zero;
-                    if (brake_left && last_brake_tick >= 0.2)
-                    {
-                        brake_rotate.z = brake_difference + Random.Range(-brake_difference * brake_turbulence_coefficient, brake_difference * brake_turbulence_coefficient);
-                        brake_left = false;
-                    }
-                    else if (!brake_left && last_brake_tick >= 0.2)
-                    {
-                        brake_rotate.z = -(brake_difference + Random.Range(-brake_difference * brake_turbulence_coefficient, brake_difference * brake_turbulence_coefficient));
-                        brake_left = true;
-                    }
-                    transform.Rotate(brake_rotate);
                     rb.AddForce((transform.up * -acceleration) * brake_force);
                 }
                 else
                 {
                     rb.AddForce(transform.up * -acceleration / 3);
                 }
-
-
             }
         }
     }
 
-    float GetTorqueDependingOnSpeed()
+    float GetTorque()
     {
         float speed = rb.velocity.sqrMagnitude;
         float torque = 0f;
 
-        for(int i = 0; i < speed; i++)
+        for (int i = 0; i < speed; i++)
         {
-            if(i < 7 && i >= 1)
+            if (i < 8 && i >= 3)
             {
                 torque += 0.2f * torque_coefficient;
             }
-            else if(i >= 7 && i < max_speed / 2)
+            else if (i >= 8 && i < max_speed / 2)
             {
-                torque += 0.02f * torque_coefficient;
+                torque += 0.03f * torque_coefficient;
             }
-            else if(i >= max_speed / 2)
+            else if (i >= (max_speed / 2))
             {
-                torque -= 0.03f * torque_coefficient;
+                torque -= 0.04f * torque_coefficient;
             }
         }
 
-        if(gas)
+        if (gas)
         {
-            torque /= 1.5f;
+            torque /= 1.6f;
         }
         return torque;
+    }
+
+
+    private float GetHitCoefficient(float angle)
+    {
+        float hit_coefficient = 1f;
+        for (int i = 0; i < angle; i++)
+        {
+            if (i <= 30 || i >= 150)
+            {
+                hit_coefficient += 0.05f;
+            }
+            else if (i >= 60 && i <= 90)
+            {
+                hit_coefficient += 0.08f;
+            }
+            else if (i >= 90 && i <= 120)
+            {
+                hit_coefficient -= 0.08f;
+            }
+            else
+            {
+                hit_coefficient -= 0.05f;
+            }
+        }
+        return hit_coefficient;
     }
 }
